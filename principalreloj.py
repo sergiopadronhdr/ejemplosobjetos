8cd8f8b7e0d04a704bc38eec84ff5fc29ef02c9d
fsanleon <52199609+fsanleon@users.noreply.github.com>
2019-06-27 13:47:41 +0100

    segunda subida ejemplo objetos

---
+++ principalreloj.py
@@ -0,0 +1,15 @@
+from reloj import*
+
+puntualidad = Reloj()
+
+comprobar = False
+while comprobar == False:
+    hora = int(input("Dame la hora: "))
+    minuto = int(input("Dame los minutos: "))
+    segundo = int(input("Dame los segundos: "))
+    if puntualidad.comprobar_hora(hora, minuto, segundo)== True:
+        comprobar = True
+
+puntualidad.set_hora(hora, minuto, segundo)
+print(puntualidad.dame_hora())
+
---
+++ reloj.py
@@ -0,0 +1,20 @@
+# SERGIO PADRON CRUZ
+
+class Reloj(object):
+    hora = 0
+    minuto = 0
+    segundo = 0
+
+    def comprobar_hora(self, hora, minuto, segundo):
+        if 0 <= hora <= 23 and 0 <= minuto <= 59 and 0 <= segundo <= 59:
+            return True
+        else:
+            return False
+
+    def set_hora(self, hora, minuto, segundo):
+        self.hora = hora
+        self.minuto = minuto
+        self.segundo = segundo
+
+    def dame_hora(self):
+        return str(self.hora) + ":" + str(self.minuto) + ":" + str(self.segundo)
